26ed5d9eae8d09cdeb4ffdf1eb2135e5e409f7d3
fz <Futianzhou@hotmail.com>
2024-10-25 08:56:03 +1300

    test 7 heavier run away penalty

--- pyboy_environment/environments/pokemon/tasks/test7.py
+++ pyboy_environment/environments/pokemon/tasks/test7.py
@@ -271,8 +271,8 @@ class PokemonBrock(PokemonEnvironment):
         if not new_state["location"]["map"] in self.ALLOWED_MAP:
             reward =  -200
 
-        if new_state['location']['map'] == "OAKS_LAB,":
-            reward -= 1
+        # if new_state['location']['map'] == "OAKS_LAB,":
+        #     reward -= 1
             
         if raw_xp_reward == 0:
             self.no_objective_cnt += 1
